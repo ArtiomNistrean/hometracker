dae68965baec42fd7cfa1b5e787960a650a28326
artiomNistrean <artiom.nistrean95@googlemail.com>
2015-11-24 17:28:37 +0000

    Added Layouts and moved to the LRC

---
+++ app/src/main/java/com/artiomnist/hometracker/MapController.java
@@ -0,0 +1,18 @@
+package com.artiomnist.hometracker;
+
+import com.google.android.gms.maps.GoogleMap;
+
+/**
+ * Created on 24/11/2015.
+ * Controller Class for the Map.
+ */
+public class MapController {
+
+    private GoogleMap map = null;
+
+
+    public GoogleMap getMap() {
+        return map;
+    }
+
+}
